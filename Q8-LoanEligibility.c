8c85925a24d26091eb51ff5a824a779bb2c4af6c
Manish <manishkolluru3024@icloud.com>
2026-09-01 10:10:30 +0530

    Q8-LoanEligibility c program

---
+++ Q8-LoanEligibility.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    float income;
+    int creditScore;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    printf("Enter income: ");
+    scanf("%f", &income);
+
+    printf("Enter credit score: ");
+    scanf("%d", &creditScore);
+
+    if (age >= 21 && age <= 60 &&
+        income > 35000 &&
+        creditScore >= 750)
+    {
+        printf("\nPerson is eligible for the loan.\n");
+    }
+    else
+    {
+        printf("\nPerson is not eligible for the loan.\n");
+    }
+
+    return 0;
+}
